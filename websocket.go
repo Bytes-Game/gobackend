ea0934f24e0663b5ad88708d1fc08b7e94082be3
Bytes-Game <nagpalkaran2001@gmail.com>
2025-12-11 11:56:24 +0530

    Implement WebSocket handler for user connections

---
+++ websocket.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[string]*websocket.Conn)
+var clientsMu sync.Mutex
+
+func WebSocketHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	username := params.Get("username")
+	if username == "" {
+		http.Error(w, "Username is required", http.StatusBadRequest)
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error upgrading to WebSocket:", err)
+		return
+	}
+
+	clientsMu.Lock()
+	clients[username] = conn
+	clientsMu.Unlock()
+
+	log.Printf("User %s connected via WebSocket", username)
+
+	// Keep the connection open
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("User %s disconnected from WebSocket", username)
+			clientsMu.Lock()
+			delete(clients, username)
+			clientsMu.Unlock()
+			break
+		}
+	}
+}
