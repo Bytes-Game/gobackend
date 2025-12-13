1db57deb2ec24749e8e244a856ab6a1edc25d0f2
Bytes-Game <nagpalkaran2001@gmail.com>
2025-12-13 14:22:07 +0530

    Update database.go

--- database.go
+++ database.go
@@ -2,21 +2,24 @@ package main
 
 import "sync"
 
-// We use a simple slice to store users for this example.
-// The User struct itself is now defined in models.go
+// users is the in-memory database for our application.
+// The User struct is defined in models.go
 var users []User
+
+// usersDBMu is a mutex to make access to the users slice safe in a concurrent environment.
 var usersDBMu sync.Mutex
 
-// InitDatabase populates our in-memory user database.
+// InitDatabase populates the in-memory user database with sample data.
 func InitDatabase() {
 	usersDBMu.Lock()
 	defer usersDBMu.Unlock()
 
-	// Create some dummy users for login testing using the central User struct.
+	// Create some dummy users for login and search testing.
+	// The User struct now includes more fields from models.go.
 	users = []User{
-		{Username: "player1", Password: "pass1", Name: "Player One"},
-		{Username: "player2", Password: "pass2", Name: "Player Two"},
-		{Username: "player3", Password: "pass3", Name: "Player Three"},
+		{Username: "player1", Password: "pass1", Name: "Player One", Followers: 150, Location: "USA"},
+		{Username: "player2", Password: "pass2", Name: "Player Two", Followers: 2500, Location: "Canada"},
+		{Username: "player3", Password: "pass3", Name: "Player Three", Followers: 1, Location: "USA"},
 	}
 }
 
