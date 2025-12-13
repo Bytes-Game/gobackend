8609b206669da065fc011a758047bd29a484b767
Bytes-Game <nagpalkaran2001@gmail.com>
2025-12-13 17:16:04 +0530

    Update models.go

--- models.go
+++ models.go
@@ -1,21 +1,19 @@
 package main
 
-import "time"
-
 // User is the central struct for a user's profile data.
-// It is used for the database and for API responses.
+// This struct has been updated to exactly match the Flutter UserModel.
 type User struct {
-	Username          string    `json:"username"`
-	password          string    // This field is unexported and will not be sent in JSON responses.
-	Name              string    `json:"name"`
-	League            string    `json:"league,omitempty"`
-	Followers         int       `json:"followers,omitempty"`
-	Wins              int       `json:"wins,omitempty"`
-	PostsCount        int       `json:"postsCount,omitempty"`
-	Caption           string    `json:"caption,omitempty"`
-	Location          string    `json:"location,omitempty"`
-	LastLogin         time.Time `json:"-"` // Explicitly exclude from JSON
-	MutualConnections []string  `json:"mutualConnections,omitempty"`
+	Username      string   `json:"username"`
+	password      string   // This field is unexported and will not be sent in JSON responses.
+	FullName      string   `json:"fullName"`
+	Caption       string   `json:"caption"`
+	Followers     int      `json:"followers"`
+	Following     int      `json:"following"`
+	Posts         int      `json:"posts"`
+	Wins          int      `json:"wins"`
+	Losses        int      `json:"losses"`
+	League        string   `json:"league"`
+	FollowingList []string `json:"followingList"`
 }
 
 // SearchResponse defines the structure for the /search endpoint's JSON response.
