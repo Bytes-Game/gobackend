b10d338ba264eb2ff286910c98ba9e3f1dd26d72
Bytes-Game <nagpalkaran2001@gmail.com>
2026-02-15 18:44:38 +0530

    Update models.go

--- models.go
+++ models.go
@@ -1,32 +1,18 @@
 package main
 
-import "time"
-
-// Post represents a single post made by a user, matching the client's expectations.
-type Post struct {
-	ID        string    `json:"id"`
-	URL       string    `json:"url"`
-	Caption   string    `json:"caption"`
-	Timestamp time.Time `json:"timestamp"`
-}
-
 // User represents a user's profile data, with the Posts field corrected.
 type User struct {
 	Username      string   `json:"username"`
 	password      string   `json:"-"` // The '-' tag prevents the password from being serialized into JSON.
 	FullName      string   `json:"fullName"`
-	Caption       string   `json:"caption"`
 	Followers     int      `json:"followers"`
-	Following     int      `json:"following"`
-	Posts         []Post   `json:"posts"` // Corrected: This is now a slice of Post objects.
+	FollowingList []string `json:"followingList"`
 	Wins          int      `json:"wins"`
 	Losses        int      `json:"losses"`
 	League        string   `json:"league"`
-	FollowingList []string `json:"followingList"`
 }
 
 // SearchResponse is a temporary struct for the search handler.
 type SearchResponse struct {
 	Results []User `json:"results"`
-	Total   int    `json:"total"`
 }
