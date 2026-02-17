2d49d522c8696d69258f4605c1f516b7ca494bfd
Bytes-Game <nagpalkaran2001@gmail.com>
2026-02-18 00:08:45 +0530

    Update models.go

--- models.go
+++ models.go
@@ -1,7 +1,10 @@
 package main
 
+import "time"
+
 // User represents a user's profile data, with the Posts field corrected.
 type User struct {
+	ID            string   `json:"id" bson:"_id,omitempty"` // Example for MongoDB
 	Username      string   `json:"username"`
 	password      string   `json:"-"` // The '-' tag prevents the password from being serialized into JSON.
 	FullName      string   `json:"fullName"`
@@ -15,4 +18,28 @@ type User struct {
 // SearchResponse is a temporary struct for the search handler.
 type SearchResponse struct {
 	Results []User `json:"results"`
+}
+
+// Represents the incoming payload for a /follow request
+type FollowEventPayload struct {
+	FollowerID        string    `json:"followerId"`
+	FollowerUsername  string    `json:"followerUsername"`
+	FollowingID       string    `json:"followingId"`
+	FollowingUsername string    `json:"followingUsername"`
+	ClientTimestamp   time.Time `json:"clientTimestamp"` // Use time.Time for proper parsing
+}
+
+// Represents the incoming payload for an /unfollow request
+type UnfollowEventPayload struct {
+	UnfollowerID        string    `json:"unfollowerId"`
+	UnfollowerUsername  string    `json:"unfollowerUsername"`
+	UnfollowedID        string    `json:"unfollowedId"`
+	UnfollowedUsername  string    `json:"unfollowedUsername"`
+	ClientTimestamp     time.Time `json:"clientTimestamp"`
+}
+
+// Notification represents a message to be sent to a user.
+type Notification struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
 }
