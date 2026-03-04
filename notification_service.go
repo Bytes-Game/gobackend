b7f17a402d78be101321d6807ed07ba6a753ecfd
Bytes-Game <nagpalkaran2001@gmail.com>
2026-03-05 00:53:12 +0530

    Refactor notification sending logic and add new notifications

--- notification_service.go
+++ notification_service.go
@@ -4,6 +4,7 @@ import (
 "encoding/json"
 "fmt"
 "log"
+"time"
 )
 
 // SendFollowNotification creates and sends a notification when a user follows another.
@@ -16,23 +17,61 @@ recipientUsername := payload.FollowingUsername
 notification := Notification{
 Type:    "follow",
 Message: fmt.Sprintf("%s started following you.", payload.FollowerUsername),
+Timestamp: time.Now().UTC().Format(time.RFC3339),
 }
 
-// Check if the user is connected via Websocket.
-conn, isOnline := IsUserOnline(recipientUsername)
+deliverNotification(recipientUsername, notification)
+}
 
-if isOnline && conn != nil {
-log.Printf("User %s is ONLINE. Sending notification directly.", recipientUsername)
-// Send the notification over the WebSocket.
-notificationJSON, _ := json.Marshal(notification)
-if err := conn.WriteMessage(1, notificationJSON); err != nil {
-log.Printf("Error sending notification to %s: %v", recipientUsername, err)
+// SendLikeNotification sends a notification when someone likes a post.
+func SendLikeNotification(likerUsername, postAuthorUsername, caption string) {
+	// Truncate caption for display
+	displayCaption := caption
+	if len(displayCaption) > 40 {
+		displayCaption = displayCaption[:40] + "..."
+	}
+
+	notification := Notification{
+		Type:		"like",
+		Message: 	fmt.Sprintf("%s liked your post: \"%s\"", likerUsername, displayCaption),
+		Timestamp: 	time.Now().UTC().Format(time.RFC3339),
+	}
+
+	deliverNotification(postAuthorUsername, notification)
 }
-} else {
-log.Printf("User %s is OFFLINE. Storing notification.", recipientUsername)
-// Store the notification for later delivery.
-StoreNotificationInRedis(recipientUsername, notification)
+
+// SendCommentNotification sends a notification when someone comments on a post.
+func SendCommentNotification(commenterUsername, postAuthorUsername, commentText, caption string) {
+	// Truncate for display
+	displayComment := commentText
+	if len(displayComment) > 50 {
+		displayComment = displayComment[:50] + "..."
+	}
+
+	notification := Notification{
+		Type:	   "comment",
+		Message:   fmt.Sprintf("%s commented on your post: \"%s\"", commenterUsername, displayComment),
+		Timestamp: time.Now().UTC().Format(time.RFC3339),
+	}
+
+	deliverNotification(postAuthorUsername, notification)
 }
+
+// deliverNotification is a helper that sends a notification to a user
+// (directly if online, stored for later if offline).
+func deliverNotification(recipientUsername string, notification Notification) {
+	conn, isOnline := IsUserOnline(recipientUsername)
+
+	if isOnline && conn != nil {
+		log.Printf("User %s is ONLINE. Sending notification directly.", recipientUsername)
+		notificationJSON, _ := json.Marshal(notification)
+		if err := conn.WriteMessage(1, notificationJSON); err != nil {
+			log.Printf("Error sending notification to %s: %v", recipientUsername, err)
+		}
+	} else {
+		log.Printf("User %s is OFFLINE. Storing notification.", recipientUsername)
+		StoreNotificationInRedis(recipientUsername, notification)
+	}
 }
 
 // SendStoredNotifications is called when a user connects via WebSocket
@@ -62,6 +101,7 @@ log.Printf("Error sending stored notification to %s: %v", username, err)
 log.Printf("Successfully sent stored notification to %s", username)
 }
 }
+
 // After sending all notifications, clear the stored notifications
 ClearStoredNotifications(username)
 log.Printf("Cleared stored notifications for %s.", username)
